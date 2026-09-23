ea641947f1911977e466916b33ab0276a74e9217
projectkarl <97941350+projectkarl@users.noreply.github.com>
2026-09-23 17:07:04 +0800

    0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "neul-v0.40.14-venue-calibration-verified316";
+const VERSION = "neul-v0.40.14-venue-calibration-verified316-news-mobile-fix1";
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = [
